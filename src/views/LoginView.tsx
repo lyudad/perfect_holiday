5b72a0975fd3ca6c887675aca09f916077902017
Holovashchenko <holovashcenko@gmail.com>
2021-10-30 13:47:26 +0300

    Added form with two inputs (lolin, password) and button to LoginView.tsx using ant-design

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,58 @@
-const LoginView = () => {
-    
+import React from "react";
+import { Form, Input, Button, Row as AntRow } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import "antd/dist/antd.css";
+
+interface ILoginVars {
+    BG: string
+};
+
+const LOGIN_VARS: ILoginVars = {
+    BG: "rgb(8 8 8 / 7%)"
+};
+
+const Row: typeof AntRow = styled(AntRow)`
+    height: 100vh;
+    background: ${LOGIN_VARS.BG};
+`;
+
+const LoginView = (): JSX.Element => {
+    return (
+        <Row justify="center" align="middle" >
+            <Form
+                name="loginForm"
+                layout="vertical"
+                size="large"
+            >
+                <Form.Item
+                    name="login"
+                    rules={[{
+                        type: "email",
+                        required: true,
+                        message: "Please enter your email address as your login!"
+                    }]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Login" />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[{
+                        required: true,
+                        message: "Please enter your password!"
+                    }]}
+                >
+                    <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 7 ,span: 17 }}>
+                    <Button type="primary" shape="round" htmlType="submit">
+                        SIGN IN
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Row>
+    )
 }
 export default LoginView;
