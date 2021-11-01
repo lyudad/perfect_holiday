fa8f0cae3a765a0136c2d784f089e7029166a45a
Alex <tverdovskyi92@gmail.com>
2021-11-01 18:32:31 +0200

    Added admin dashbord page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import Container from './Components/Container';
+import { Switch, Route } from "react-router-dom";
 
-function App() {
+import Container from "./Components/Container";
+
+import AdminView from "./views/AdminView/AdminView";
+
+function App(): JSX.Element {
   return (
     <Container>
-    
+      <Switch>
+        <Route exact path="/admin-page" component={AdminView} />
+      </Switch>
     </Container>
   );
 }
