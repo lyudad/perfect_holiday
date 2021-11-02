a90b1474879a3627262013f068eb5d4b493b9d71
Yaroslav <yaroslav.brila@gmail.com>
2021-11-02 11:17:49 +0200

    changed filename en.tsx

--- src/views/user/index.tsx
+++ src/views/user/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import {lang} from "../../language/en";
+import {lang} from "../../language/english";
 import {Row, Input, Form} from 'antd';
 import {Table} from 'antd';
 import {
