a818832b9cc67135c0658295be2716d814631806
Serhii Zadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-11-04 18:02:18 +0200

    clear code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Container from './Components/Container';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <Container>
-    
+      <Sidebar />
     </Container>
   );
 }
--- src/Components/Sidebar/styled.tsx
+++ src/Components/Sidebar/styled.tsx
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
+const Border = {
+    color: "#bfbfbf",
+}
+
+const border = { #bfbfbf };
+
+const Background = {
+    backgroundColor: "#f5f5f5",
+}
+
+const BackgroundHover = {
+    bacgroundColor: "#d9d9d9",
+}
+
+
 const SidebarBlock = styled.div`
-    border: 1px solid #bfbfbf;
+    border: 1px solid {<Border>||<color>};
+    
     border-radius: 2px;
     width: 200px;
     height: 300px;
@@ -11,9 +27,7 @@ const SidebarBlock = styled.div`
 const SidebarUl = styled.ul`
     display: flex;
     flex-flow: column;
-    /* justify-content: space-between; */
     height: 100%;
-
     margin: 0px;
     padding: 0px;
 `;
@@ -27,11 +41,9 @@ const SidebarLi = styled.li`
     background: #f5f5f5;
     margin-bottom: 5px;
     height: 100%;
-
     display: flex;
     justify-content: center;
     align-items: center;
-
     font-size: 18px;
 
     :last-child {
