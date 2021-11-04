d44335fb02c631f34929548d5ed8c585162474ed
Alex <73483900+TverdovskyiAleksey@users.noreply.github.com>
2021-11-04 16:04:58 +0200

    Update styles.ts

--- src/views/login/styles.ts
+++ src/views/login/styles.ts
@@ -1,6 +1,6 @@
 import { Row as AntRow } from "antd";
 import styled from "styled-components";
-import { LOGIN_VARS } from "../../constants/styles";
+import { LOGIN_VARS } from "~/constants/styles";
 
 
 export const Row: typeof AntRow = styled(AntRow)`
