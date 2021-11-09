31bd6e5aa9cb6c8bf131fd05bb265939e2e94dd3
Alex <73483900+TverdovskyiAleksey@users.noreply.github.com>
2021-11-09 15:28:16 +0200

    Update AdminView.tsx
    delete function, fix future

--- src/views/AdminView/AdminView.tsx
+++ src/views/AdminView/AdminView.tsx
@@ -4,14 +4,12 @@ import { StyledLayout, StyledContent } from "./styles";
 import { columns, data } from "./constants";
 
 const AdminView = (): JSX.Element => {
-  function onChange(pagination: any, filters: any, sorter: any, extra: any) {
-    console.log("params", pagination, filters, sorter, extra);
-  }
+
 
   return (
     <StyledLayout>
       <StyledContent>
-        <Table columns={columns} dataSource={data} onChange={onChange} />
+        <Table columns={columns} dataSource={data}/>
       </StyledContent>
     </StyledLayout>
   );
