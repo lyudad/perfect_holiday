07093fbb372d17e272ced6e90e669e345b5548f4
Holovashchenko <holovashcenko@gmail.com>
2021-11-09 03:16:46 +0200

    Added relative path

--- src/Components/Access/hooks.ts
+++ src/Components/Access/hooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import type { RootState } from '~/Redux/store';
+import type { RootState } from 'Redux/store';
 
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
