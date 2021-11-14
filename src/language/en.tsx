f46d68ef6570ae3f7a1db41326a34393fcc4deba
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-11-14 14:05:18 +0200

    Added text for IS USER BLOCK control

--- src/language/en.tsx
+++ src/language/en.tsx
@@ -1,21 +1,26 @@
 export const lang = {
-    login: {
-        "email-validation": "Please enter your email address as your login!",
-        "password-validation": "Please enter your password!"
-    },
-    username: {
-        "firstName-validation": "Please enter your first name!",
-        "lastName-validation": "Please enter your last name!"
-    },
-    profile:{
-        "profile":"Profile",
-        "logout":"Logout"
-    },
-    sidebar: {
-        "dashboard": "Dashboard",
-        "users": "Users",
-        "profile": "Profile",
-        "logout": "Logout",
-    }
-
+  login: {
+    'email-validation': 'Please enter your email address as your login!',
+    'password-validation': 'Please enter your password!',
+  },
+  username: {
+    'firstName-validation': 'Please enter your first name!',
+    'lastName-validation': 'Please enter your last name!',
+  },
+  profile: {
+    profile: 'Profile',
+    logout: 'Logout',
+  },
+  sidebar: {
+    dashboard: 'Dashboard',
+    users: 'Users',
+    profile: 'Profile',
+    logout: 'Logout',
+  },
+  updateStatus: {
+    success: "User's data was successfully updated!",
+    fail: 'Something goes wrong!',
+    block: 'Block',
+    unblock: 'Unblock',
+  },
 }
