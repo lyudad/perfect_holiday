eb5e330279ba3a07e9f814c60b58475b65db233f
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-11-14 14:07:24 +0200

    Realized IS_BLOCK func, for change status block | unblock

--- src/Components/Users/index.tsx
+++ src/Components/Users/index.tsx
@@ -1,37 +1,46 @@
-import { useState } from "react";
-import { Table, Space } from "antd";
-import "antd/dist/antd.css";
-import { StyledButton, StyledContent, StyledLayout } from "./styles";
-import Sidebar from "../Sidebar";
-import useUsers from "hooks/useUsers";
-import Loading from "Components/Loading";
-const { Column } = Table;
+import React from 'react'
+import { Table, Space, message, Button } from 'antd'
+import 'antd/dist/antd.css'
+import { StyledButton, StyledContent, StyledLayout } from './styles'
+import Sidebar from '../Sidebar'
+import useUsers from 'hooks/useUsers'
+import Loading from 'Components/Loading'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { lang } from 'language/en'
+const { Column } = Table
 
-const data = [
-  {
-    key: 1,
-    first_name: "John",
-    is_block: false,
-  }, 
-  {
-    key: 2,
-    first_name: "Jim",
-    is_block: false,
-  },
-  {
-    key: 3,
-    first_name: "Joe",
-    is_block: false,
-  },
-];
+// const data = [
+//   {
+//     key: 1,
+//     first_name: "John",
+//     is_block: false,
+//   },
+//   {
+//     key: 2,
+//     first_name: "Jim",
+//     is_block: false,
+//   },
+//   {
+//     key: 3,
+//     first_name: "Joe",
+//     is_block: false,
+//   },
+// ];
 
-const Users = () => {
-  const [toggle, setToggle] = useState(false);
-  // Здесь приходит обьект data который будем ставить вместо моковых данных
-  // Запускал данные с БД поддягиваються
-  // const { error, isLoading, data } = useUsers();
-  // if (isLoading) return <Loading />
-  // if (error instanceof Error) return <h1>Error: {error.message}</h1>
+const Users = (): JSX.Element => {
+  // Достает всех users
+  const { error, isLoading, data } = useUsers()
+  if (isLoading) return <Loading />
+  if (error instanceof Error) return <h1>Error: {error.message}</h1>
+
+  // Меняет is_block true / false
+  const { REACT_APP_USERS_LIST } = process.env
+  const goBlock = async (dataIndex: boolean, key: any) =>
+    axios
+      .put(`${REACT_APP_USERS_LIST}${key.id}`, { is_block: !dataIndex })
+      .then(() => message.success(lang.updateStatus.success))
+      .catch(() => message.error(lang.updateStatus.fail))
 
   return (
     <StyledLayout>
@@ -46,31 +55,38 @@ const Users = () => {
           +
         </StyledButton>
         <Table dataSource={data}>
-          <Column title="First Name" dataIndex="first_name" key="first_name" />
+          <Column title="Last Name" dataIndex="last_name" key="id" />
           <Column
             title="Action"
             key="action"
-            render={() => (
+            dataIndex="id"
+            render={(dataIndex) => (
               <Space size="middle">
-                <a>Edit</a>
+                <Link to={`/admin/${dataIndex}`}>Edit</Link>
               </Space>
             )}
           />
           <Column
             title="User status"
-            dataIndex="status"
-            key="first_name"
-            render={() => (
+            dataIndex="is_block"
+            key="id"
+            render={(dataIndex, key) => (
               <Space size="middle">
-                <a onClick={() => setToggle((toggle) => !toggle)}>
-                  {toggle ? "block" : "unblock"}
-                </a>
+                <Button
+                  onClick={() => goBlock(dataIndex, key)}
+                  htmlType="submit"
+                  type="link"
+                >
+                  {dataIndex
+                    ? lang.updateStatus.block
+                    : lang.updateStatus.unblock}
+                </Button>
               </Space>
             )}
           />
         </Table>
       </StyledContent>
     </StyledLayout>
-  );
-};
-export default Users;
+  )
+}
+export default Users
