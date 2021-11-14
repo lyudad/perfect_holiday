2f721095933dc4c14dc5e36fdfc3e01fd4832961
Holovashchenko Vladyslav <holovashcenko@gmail.com>
2021-11-14 14:01:33 +0200

    Refactoting

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,27 +1,22 @@
-import { useQuery } from "react-query";
-import axios from "axios"
-const {REACT_APP_USERS_LIST} = process.env
-export type UserRoleType = 'employee' | 'admin' | 'super';
+import { useQuery } from 'react-query'
+import axios from 'axios'
+export type UserRoleType = 'employee' | 'admin' | 'super'
+const { REACT_APP_USERS_LIST } = process.env
 
 export type User = {
-  role: UserRoleType;
-  email: string;
-  password: string;
-  first_name: string;
-  last_name: string;
-  is_block: boolean;
-  available_vacation: number;
-  available_sick_days: number;
+  role: UserRoleType
+  email: string
+  password: string
+  first_name: string
+  last_name: string
+  is_block: boolean
+  available_vacation: number
+  available_sick_days: number
 }
 
 export default function useUsers() {
-  return useQuery(
-    "users",
-    async (): Promise<Array<User>> => {
-      const { data } = await axios.get(
-        `${REACT_APP_USERS_LIST}`
-      );
-      return data;
-    }
-  );
+  return useQuery('users', async (): Promise<Array<User>> => {
+    const { data } = await axios.get(`${REACT_APP_USERS_LIST}`)
+    return data
+  })
 }
