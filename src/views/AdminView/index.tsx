21476f017e6ea3fb48f4d87f0f72fdab0b3d40d3
Alex <tverdovskyi92@gmail.com>
2021-12-12 11:24:42 +0200

    changes

--- src/views/AdminView/index.tsx
+++ src/views/AdminView/index.tsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Sidebar from 'Components/Sidebar';
 import { lang } from 'language/en';
 import { columns } from './const';
 import Layout from './layout';
-import { toUpdateUserInfo } from 'hooks/useUsers';
+import useGetListOfUsers, {
+  getUserRequestDays,
+  toUpdateUserInfo,
+} from 'hooks/useUsers';
 import 'antd/dist/antd.css';
 import { Row, Input, Form, Col, Button, Table, message, Select } from 'antd';
 import {
@@ -13,7 +16,7 @@ import {
   StyledDivContent,
   StyledDivVacationInfo,
   ButtonWrapper,
-  SelectBlock
+  SelectBlock,
 } from './styles';
 import { url } from 'constants/constants';
 import { useRouteMatch } from 'react-router-dom';
@@ -22,49 +25,28 @@ import { sellectItemColor } from 'constants/constants';
 import axios from 'axios';
 import store from 'Redux/store';
 import { Role } from 'constants/constants';
+import { User } from 'hooks/types';
 
 const { REACT_APP_BASE } = process.env;
 const { Option } = Select;
-const data = [
-  {
-    key: '1',
-    month: 'June 2020',
-    dates: '10-20',
-    status: 'approved',
-    type: 'vacation',
-  },
-  {
-    key: '2',
-    month: 'April 2020',
-    dates: '01-09',
-    status: 'approved',
-    type: 'vacation',
-  },
-  {
-    key: '3',
-    month: 'January 2020',
-    dates: '08-10',
-    status: 'pending',
-    type: 'vacation',
-  },
-  {
-    key: '4',
-    month: 'February 2019',
-    dates: '08',
-    status: 'pending',
-    type: 'sick leave',
-  },
-];
 
 const AdminView = (): JSX.Element => {
   const [form] = Form.useForm();
-  const [type, setType] = useState<string>('employee');
   const state = store.getState();
+  // console.log(state);
+
+  const userId = useRouteMatch<IMatchParams>().params.id;
+  console.log(userId);
+
+  const { data } = useGetListOfUsers();
+  // const { data } = getUserRequestDays(userId);
+  console.log(data);
+
   const role = state.person.user.role;
   const InitialState = {
-    canSelectRoleInEdit: (role === Role.SUPER)
+    canSelectRoleInEdit: role === Role.SUPER,
   };
-  const userId = useRouteMatch<IMatchParams>().params.id;
+
   const updateUserInfo = () => {
     toUpdateUserInfo(form.getFieldsValue(), userId)
       .then(() => message.success(lang.updateStatus.success))
@@ -101,7 +83,7 @@ const AdminView = (): JSX.Element => {
               onFinish={updateUserInfo}
               size="large"
             >
-              <Row justify="space-between"> 
+              <Row justify="space-between">
                 <Col span={6}>
                   <Form.Item name="first_name" rules={[{ type: 'string' }]}>
                     <Input placeholder={lang.userInfo.firstName} />
@@ -117,24 +99,19 @@ const AdminView = (): JSX.Element => {
                     <Input placeholder={lang.userInfo.email} />
                   </Form.Item>
                 </Col>
-                {
-                  (InitialState.canSelectRoleInEdit)
-                  &&
+                {InitialState.canSelectRoleInEdit && (
                   <Col span={3}>
                     <Form.Item name="role" rules={[{ type: 'string' }]}>
-                      <SelectBlock
-                        placeholder={lang.superAdmin.roleTitle}
-                      >
-                        <Option value="admin" key="id" >
+                      <SelectBlock placeholder={lang.superAdmin.roleTitle}>
+                        <Option value="admin" key="id">
                           Admin
                         </Option>
-                        <Option value="employee">
-                          Employee
-                        </Option>
+                        <Option value="employee">Employee</Option>
                       </SelectBlock>
                     </Form.Item>
+                    //{' '}
                   </Col>
-                }
+                )}
                 <Col span={4}>
                   <Form.Item>
                     <Button
@@ -149,15 +126,44 @@ const AdminView = (): JSX.Element => {
                 </Col>
               </Row>
             </Form>
-
-            <Row>
-              <StyledDivVacationInfo>
-                <strong>2 sick leave</strong>
-              </StyledDivVacationInfo>
-              <StyledDivVacationInfo>
-                <strong>14 vacation days</strong>
-              </StyledDivVacationInfo>
-            </Row>
+            {data?.map(
+              ({
+                id,
+                first_name,
+                available_sick_days,
+                available_vacation,
+                last_name,
+                email,
+              }: User) => (
+                <div>
+                  {id == userId ? (
+                    <Row>
+                      <StyledDivVacationInfo>
+                        <strong>{first_name}</strong>
+                      </StyledDivVacationInfo>
+                      <StyledDivVacationInfo>
+                        <strong>{last_name}</strong>
+                      </StyledDivVacationInfo>
+                      <StyledDivVacationInfo>
+                        <strong>{email}</strong>
+                      </StyledDivVacationInfo>
+                    </Row>
+                  ) : null}
+                  {id == userId ? (
+                    <Row>
+                      <StyledDivVacationInfo>
+                        <strong>{available_sick_days}</strong>
+                        <strong>{' sick days'}</strong>
+                      </StyledDivVacationInfo>
+                      <StyledDivVacationInfo>
+                        <strong>{available_vacation}</strong>
+                        <strong>{' vacasion days'}</strong>
+                      </StyledDivVacationInfo>
+                    </Row>
+                  ) : null}
+                </div>
+              ),
+            )}
           </StyledDivContent>
           <ButtonWrapper>
             <StyledButton
@@ -172,6 +178,7 @@ const AdminView = (): JSX.Element => {
               {lang.button['addButton']}
             </StyledButton>
           </ButtonWrapper>
+
           <Table
             columns={columns}
             dataSource={data}
