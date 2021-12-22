7d6ae614abad5a6c6ff62aa6ed0b2a7866f36f0c
Yaroslav <yaroslav.brila@gmail.com>
2021-12-22 13:03:43 +0200

    Update DiffDays.tsx

--- src/Components/Functions/DiffDays.tsx
+++ src/Components/Functions/DiffDays.tsx
@@ -1,5 +1,5 @@
 const MS_PER_DAY = 1000 * 60 * 60 * 24;
-export const dateDiffInDays = (a: Date, b: Date)=> {
+export const dateDiffInDays = (a: Date, b: Date): number => {
   const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
   const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
