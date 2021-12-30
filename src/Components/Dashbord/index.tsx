87a430da899f55d7263393348d2d6cfd300b17ea
Yaroslav <yaroslav.brila@gmail.com>
2021-12-30 12:49:46 +0200

    Update index.tsx

--- src/Components/Dashbord/index.tsx
+++ src/Components/Dashbord/index.tsx
@@ -67,11 +67,8 @@ const Dashbord = (): JSX.Element => {
       });
   };
   const onSubmit = () => {
-
     if (!watchAll.startDate) start_date= showCurrentDate(startDate)
-
     if (!watchAll.endDate) end_date= showCurrentDate(endDate)
-
     const difference = dateDiffInDays(new Date(start_date), new Date(end_date))+1;
     console.log( start_date,end_date,difference)
     toEditRestDays({
